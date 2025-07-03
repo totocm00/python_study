41afc0af5d7fada7aa48db10fbe97a9052975859
WonMyeong Yang <typesapk@naver.com>
2025-07-03 15:52:32 +0900

    오늘 문제 1번 커밋

---
+++ python/문제0703.py
@@ -0,0 +1,16 @@
+ii1 = int(input("첫번째 정수를 입력하세요"))
+ii2 = int(input("두번째 정수를 입력하세요"))
+sum1 = ii1 + ii2
+minus1 = ii1 - ii2
+mul1 = ii1 * ii2
+ave1 = (ii1+ii2) / 2
+max1 = max(ii1,ii2)
+min1 = min(ii1,ii2)
+print("합은 : ", sum1)
+print("차는 : ", minus1)
+print("곱은 : ", mul1)
+print("평균은 : ", ave1)
+print("큰 수 : ", max1)
+print("작은 수 : ", min1)
+
+print(sum1,minus1,mul1,ave1,max1,min1)
