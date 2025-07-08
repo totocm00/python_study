c106c970a313251640fe15213abff741bbecccb3
totocm00 <typesapk@naver.com>
2025-07-08 23:52:02 +0900

    refactor: 배수문제 삼항연산자 변환

--- python/individual_ex/multiple_3_5.py
+++ python/individual_ex/multiple_3_5.py
@@ -2,12 +2,19 @@
 # 만약 5의 배수이면 "Express"을 출력한다.
 # 만약 3의 배수이면서 5의 배수이면 "Python Express"를 출력한다
 
+def check_multiple(num):
+    result = ""
+    if num % 3 == 0:
+        result += "Python "
+    if num % 5 == 0:
+        result += "Express"
+    print(result.strip() if result else "정수를 입력하세요")
+
 integer_input = int(input())
-if integer_input % 3 == 0 and integer_input % 5 == 0:
-    print("Python Express")
-elif integer_input % 3 == 0:
-    print("Python")
-elif integer_input % 5 == 0:
-    print("Express")
-else:
-    ("정수를 입력하세요")
+check_multiple(integer_input)
+
+# strip() 는 문장 양 끝의 공백을 제거한다
+# <A> if <조건> else <B>  ->  조건이 참이면 A, 아니면 B 반환
+# A와 B에는 내가 만든 함수를 비교식으로 넣어도 가능하다
+# 간단한 내용은 삼항연산자로 표현하면 간결하지만
+# 긴 내용은 가독성을 해치기 때문에 비추천
