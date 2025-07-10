8886702fad66b48804f25fdd2cdec7f9e0815073
totocm00 <typesapk@naver.com>
2025-07-10 09:42:42 +0900

    refactor: ex04

--- python/homework_0709.py/ex04.py
+++ python/homework_0709.py/ex04.py
@@ -7,3 +7,9 @@ for i in range(1,user_input_integer + 1):
     if user_input_integer % i == 0:
         multiple_x += str(i) + " "
 print(multiple_x)
+
+# refactor
+num = user_input_integer
+for i in range(1,num+1):
+    if num%i == 0:
+        print(i,end=" ")
