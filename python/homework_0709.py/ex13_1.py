9ad38232e4e6023a558be98a3c8b3abc323c4968
totocm00 <typesapk@naver.com>
2025-07-10 10:46:46 +0900

    refactor: ex13

--- python/homework_0709.py/ex13_1.py
+++ python/homework_0709.py/ex13_1.py
@@ -21,3 +21,24 @@ for i in range(n + 1):  # 0부터 n까지 포함되도록 n+1
         print(fn, end=' ')
         f0 = f1  # 이전 값 업데이트
         f1 = fn  # 현재 값 업데이트
+
+# refactor
+
+num = int(input())
+element1 = 0
+element2 = 0
+
+for i in range(num + 1):
+    if i<2:
+        element2 = element1
+        element1 = i
+        sum = element1 + element2
+    else:
+        sum = element1 + element2
+        element2 = element1
+        element1 = sum
+    
+    if i!=num:
+        print(sum,end=",")
+    else:
+        print(sum)
