beb38f1647c6338507d859f3ff41853f40563fcc
totocm00 <typesapk@naver.com>
2025-07-11 11:57:09 +0900

    refactor: proverb_quiz / for_in 5, correct

--- python/mk/proverb_quiz.py
+++ python/mk/proverb_quiz.py
@@ -2,42 +2,54 @@
 import random
 
 proverbs = [
-    ["굴러온 돌이","박힌 돌 뺀다"],
+    ["가까운","무당보다 먼 데 무당이 영하다"],
     ["긁어","부스럼"],
-    ["돼지에","진주 목걸이"],
+    ["경점 치고","문지른다"],
     ["꼬리가 길면","밟힌다"],
-    ["돌다리도","두들겨 보고 건너라"],
-    ["마른하늘에","날벼락"]
+    ["내 일 바빠","한댁 방아"],
+    ["망건 골에","앉았다"],
+    ["석류는 떨어져도 안 떨어지는","유자를 부러워하지 않는다"],
+    ["호박 덩굴이","벋을 적 같아서야"],
+    ["이 열 번 물어도","모르면 송구하다"],
+    ["죽 쑤고","개가 웃는다"],
+    ["호랑이의 새끼를","만들지 않는다"]
 ]
 
-quiz = random.choice(proverbs)
-front, back = quiz
+correct = 0
+for i in range(5):
 
-while True:
-    lie1, lie2 = random.choice(proverbs)
-    if front != lie1:
-        break
+    quiz = random.choice(proverbs)
+    front, back = quiz
 
-front_or_back = random.choice(quiz)
+    while True:
+        lie1, lie2 = random.choice(proverbs)
+        if front != lie1:
+            break
 
-print(f"문제 :{front_or_back}")
+    front_or_back = random.choice(quiz)
 
-if front_or_back == front:
-    front = back
+    print(f"문제{i+1} :  {front_or_back}")
 
-list_a = [front,lie1]
-random.shuffle(list_a)
+    if front_or_back == front:
+        front = back
 
-print(f"1.{list_a[0]}  2.{list_a[1]}")
-user_input = int(input("이어질 속담을 선택하세요: 1, 2 "))
+    list_a = [front,lie1]
+    random.shuffle(list_a)
 
-result = ""
-if user_input == 1:
-    result = list_a[0]
-else:
-    result = list_a[1]
+    print(f"1.{list_a[0]}\n2.{list_a[1]}")
+    user_input = int(input("이어질 속담을 선택하세요: 1, 2 "))
 
-if result == front or result == back:
-    print("정답입니다")
-else:
-    print("틀렸습니다")
+    result = ""
+    if user_input == 1:
+        result = list_a[0]
+    else:
+        result = list_a[1]
+
+    if result == front or result == back:
+        correct += 1
+        print("\t정답입니다")
+    else:
+        print("\t틀렸습니다")
+    print("---------------------")
+    
+print(f"5문제 중 {correct}개 정답")
