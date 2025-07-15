57e0a1e30ca4f72ea7efe5e5f25fc8ca8f5082c9
totocm00 <typesapk@naver.com>
2025-07-15 11:31:03 +0900

    feat: FPS/ clock.tick()

--- python/pygame/py_game00.py
+++ python/pygame/py_game00.py
@@ -5,6 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600)) # 버퍼
 pygame.display.set_caption("PY게임!") # 맨 위의 타이틀 바
 
+clock = pygame.time.Clock()
+
 # screen.fill((0,0,0)) # RGB -> 출력하라는 로직 / 그래서 한 번 보여주고 끝
 
 # 폰트 지정 / 맑은 고딕
@@ -39,3 +41,4 @@ while True:
     screen.blit(text, (x,y))
     pygame.display.update()
             
+    clock.tick(120)   # FPS 개념이 .tick = 즉, (숫자)의 FPS
