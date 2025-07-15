62b3cb8e3df13b8aea0ae6734631e67845d8c579
totocm00 <typesapk@naver.com>
2025-07-15 11:23:00 +0900

    feat: 화면에 글자 출력 blit

--- python/pygame/py_game00.py
+++ python/pygame/py_game00.py
@@ -9,6 +9,7 @@ pygame.display.set_caption("PY게임!") # 맨 위의 타이틀 바
 
 # 폰트 지정 / 맑은 고딕
 FONT = pygame.font.SysFont("malgun gothic", 48)
+# render -> 화면에 쓰기위한 함수
 text = FONT.render("Intel", True, (255,255,255))
 # ---------------------------
 
@@ -21,5 +22,10 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 pygame.quit()
-
+    
+    # block transfer
+    # : 한 이미지의 픽셀 데이터를 다른 표면(Surface)으로 복사하는 작업
+    screen.blit(text, (330,0))
+    # display에 내용을 업데이트 시킴
+    pygame.display.update()
             
