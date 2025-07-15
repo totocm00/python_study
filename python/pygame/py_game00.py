47cff32399e1b8abe7225e4790409747d7835f18
totocm00 <typesapk@naver.com>
2025-07-15 10:44:57 +0900

    feat: pygame_event_게임유지/ KEYDOWN, KEYUP : KEY 이벤트

--- python/pygame/py_game00.py
+++ python/pygame/py_game00.py
@@ -1,8 +1,20 @@
 import pygame
 
-#초기화
+#초기화 ---------------init구간
 pygame.init()
 screen = pygame.display.set_mode((800, 600)) # 버퍼
 pygame.display.set_caption("PY게임!") # 맨 위의 타이틀 바
 
-screen.fill((0,0,0)) # RGB
+# screen.fill((0,0,0)) # RGB -> 출력하라는 로직 / 그래서 한 번 보여주고 끝
+
+# ---------------------------
+
+while True:
+    screen.fill((0,0,0))
+
+    for event in pygame.event.get():
+        # KEYDOWN 이벤트 -> 눌렀을 때 실행
+        # KEYUP 이벤트 -> 뗏을 때 실행
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit()
